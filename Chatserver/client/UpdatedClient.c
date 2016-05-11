57b314e4853b7c8c09df7fb2b5d77adff2d531e5
goatboy <recon2151@gmail.com>
2016-05-11 16:10:37 +0000

    all caught up

--- Chatserver/client/UpdatedClient.c
+++ Chatserver/client/UpdatedClient.c
@@ -25,35 +25,66 @@ int main(int argc, char** argv)
     }
     else
     {
-        puts("We are Connected");
-        //Receive a reply from the server
-        int MAX_SIZE = 2000 * sizeof(char);
-        char* server_reply = malloc(MAX_SIZE);
-        
-        while(1)
-        {
-            error = recv(sockfd, server_reply, MAX_SIZE, 0);
-            if(error < 0)
-            {
-                puts("recv failed");
-            }
-            else
-            {
-                puts("Reply received\n");
-                puts(server_reply);
-                
-                //char* message = "yo\tyo";
-                
-            }
-                char* message;
-                char scanFread[100];
-                scanf("%s", scanFread);
-                message = scanFread;
-                puts("Message sent....\n");
-                send(sockfd, message , strlen(message) , 0);
-        }
+       int pid = fork();
+       if(pid == 0)
+       {
+           childStuff(sockfd);
+          
+       }
+       else 
+       {
+           parentStuff(sockfd);
+       }
     }
     free(server);
     return 0;
+}
+
+void childStuff(int sockfd)
+{
+    int error;
+    //Receive a reply from the server
+    int MAX_SIZE = 2000 * sizeof(char);
+    char* server_reply = malloc(MAX_SIZE);
+    while(1)
+    {
+        puts("Waiting to recv");
+        error = recv(sockfd, server_reply, MAX_SIZE, 0);
+        if(error < 0)
+        {
+            puts("recv failed");
+        }
+        else
+        {
+            puts(server_reply);
+            server_reply = malloc(MAX_SIZE);
+            free(server_reply);
+            //server_reply[0] = '\0';
+        }
+    }
+}
+
+void parentStuff(int sockfd)
+{
+    char* command;
+    char* who;
+    char* msg;
+    while(1)
+    {
+        scanf("%ms", &command);
+        if(strcmp("tell", command) == 0)
+        {
+            scanf("%ms %m[^\n]", &who, &msg);
+            printf("Sending private message (%s) to: %s\n", msg, who);
+        }
+        else
+        {
+            scanf("%m[^\n]", &msg);
+            char* destination = malloc(sizeof(command) + sizeof(msg));
+            strcpy(destination, command);
+            strcat(destination, msg);
+            send(sockfd, destination , strlen(destination) , 0);
+        } 
+    }
 }
 
